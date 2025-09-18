a56ab3ff701eb07e733b25c6bdf3e6bb7fde4d85
harsh-accio <rajputharsh9897@gmail.com>
2025-09-18 20:59:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function firstWord(s) {
   // your code here
+	let str='';
+	s=s.trim();
+	let i=0;
+	while(s[i]!=' ' && i<s.length){
+		str+=s[i];
+		i++;
+	}
+	return str;   
 }
 
 // Do not change the code below
